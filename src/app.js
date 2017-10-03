df5aa4fff197b010070236bb39e4523bc9fa1f79
Anirudh <adhullip@thoughtworks.com>
2017-10-03 12:08:08 +0530

    initial commit

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+let x = 3;
+const y = 4;
+class Person {}
+const title = <h1>To Do</h1>;
+ReactDOM.render(title, document.getElementById("app"))
